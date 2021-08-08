088a2ccfdb46f188ed6836bff5a065b2fa8b8c44
aka <aka@akapc.local>
2021-08-09 03:01:59 +0900

    heroku push

---
+++ app.py
@@ -0,0 +1,45 @@
+import configparser
+import os
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+CONF_PATH = './utils/params.ini'
+
+app = Flask(__name__)
+conf = configparser.ConfigParser()
+line_bot_api = LineBotApi(conf['access']['token'])
+handler = WebhookHandler(conf['access']['secret'])
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print(conf['errorlog']['invalid'])
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='湿度{}です！'.format(event.message.text)))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
